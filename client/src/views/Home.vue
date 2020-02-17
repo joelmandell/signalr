1f0ef542065f53123efce556c3f84d19637e6f37
joelmandell <joelmandell@gmail.com>
2020-02-17 10:39:59 +0100

    first commit of demo

---
+++ client/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <TaskBar />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Taskbar from '@/components/TaskBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Taskbar
+  }
+}
+</script>
